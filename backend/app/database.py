2c863c21bf1d5db4713527392dbcd90168abf1fa
vininoronha21 <contatovininoronha@gmail.com>
2026-04-03 12:48:24 -0300

    feat: database criado

---
+++ backend/app/database.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+  raise ValueError("DATABASE_URL não encontrada. Verifique o arquivo .env")
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
