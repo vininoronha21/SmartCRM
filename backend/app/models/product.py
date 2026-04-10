86afe0a7e1e4cb4f364ef31de447fbf6796695d7
vininoronha21 <contatovininoronha@gmail.com>
2026-04-09 21:20:09 -0300

    refactor: alteração no model para enxugar ETL

--- backend/app/models/product.py
+++ backend/app/models/product.py
@@ -5,14 +5,10 @@ from app.models.base import Base
 class Product(Base):
   __tablename__ = "products"
 
+  # Mantém apenas a categoria em português
+  
   product_id: Mapped[str] = mapped_column(primary_key=True)
   category_name: Mapped[str] = mapped_column(nullable=True)
-  category_name_english: Mapped[str] = mapped_column(nullable=True)
-  weight_g: Mapped[int] = mapped_column(nullable=True)
-  length_cm: Mapped[int] = mapped_column(nullable=True)
-  height_cm: Mapped[int] = mapped_column(nullable=True)
-  width_cm: Mapped[int] = mapped_column(nullable=True)
-
-  # OBS: 'category_name_english' não existe no CSV original, o que acontece é que tem
-  # um arquivo chamado 'product_category_name_translation.csv', e no ETL vou fazer o join
-  # desses dois arquivos para já salvar a tradução diretamente na tabela de PRODUTOS.
+  category_name: Mapped[str] = mapped_column(nullable=True)
+  name_length: Mapped[int] = mapped_column(nullable=True)
+  description_length: Mapped[int] = mapped_column(nullable=True)
